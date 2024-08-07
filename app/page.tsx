117dd9f0aeed4654251e77a3384184272671561c
Colin Franceschini <hello@colinfran.com>
2024-08-06 17:48:48 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const Page: FC = () => {
         />
       </div>
       <div className="my-6 flex flex-col items-center justify-center">
-        <Countdown targetDate="7-4-2025" />
+        <Countdown targetDate="7-13-2025" />
       </div>
       <div className="relative flex items-center justify-start">
         <Image
