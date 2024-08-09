b068b4f3cb12b03367d723b03d463abdc6a95460
Colin Franceschini <hello@colinfran.com>
2024-08-08 17:37:29 -0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
-  // ...other options
+  exclude: ["/icon.svg", "/apple-icon.png", "/manifest.webmanifest", "/api", "/api/*"]
 }
