67269112f9bb1c444a7410a094c3fe4b5e200dbc
Colin Franceschini <hello@colinfran.com>
2024-09-13 22:01:50 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,24 +37,24 @@ const RootLayout: React.FC<RootLayoutProps> = async ({ children }) => {
         <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         <link href="/manifest.json" rel="manifest" />
         <meta content="/ms-icon-144x144.png" name="msapplication-TileImage" />
-         
+
         {/* SEO Meta Tags */}
-        <meta name="description" content="Orne and Colin are getting married!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="https://franceschini.wedding" />
-        
+        <meta content="Orne and Colin are getting married!" name="description" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link href="https://franceschini.wedding" rel="canonical" />
+
         {/* Open Graph Meta Tags */}
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Colin & Orne" />
-        <meta property="og:description" content="Colin and Orne are getting married!" />
-        <meta property="og:url" content="https://franceschini.wedding" />
-        <meta property="og:image" content="https://i.ibb.co/dQG3QVC/image.jpg" />
+        <meta content="website" property="og:type" />
+        <meta content="Colin & Orne" property="og:title" />
+        <meta content="Colin and Orne are getting married!" property="og:description" />
+        <meta content="https://franceschini.wedding" property="og:url" />
+        <meta content="https://i.ibb.co/dQG3QVC/image.jpg" property="og:image" />
 
         {/* Twitter Card Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Colin & Orne" />
-        <meta name="twitter:description" content="Colin and Orne are getting married!" />
-        <meta name="twitter:image" content="https://i.ibb.co/dQG3QVC/image.jpg" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="Colin & Orne" name="twitter:title" />
+        <meta content="Colin and Orne are getting married!" name="twitter:description" />
+        <meta content="https://i.ibb.co/dQG3QVC/image.jpg" name="twitter:image" />
       </head>
       <body className={inter.className}>
         <div>
