b55f6349572d57be1999886f88e84a67b632606c
Colin Franceschini <hello@colinfran.com>
2025-01-15 15:40:42 -0800

    Update middleware.ts

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,3 +1,11 @@
+/**
+ * Middleware function to handle locale determination and setting.
+ * This middleware checks for a 'locale' cookie in the incoming request. If the cookie is not present,
+ * it attempts to determine the locale from the 'accept-language' header of the request. If the locale
+ * is determined from the header, it sets a 'locale' cookie for future requests.
+ * @param {NextRequest} request - The incoming request object.
+ * @returns {Promise<NextResponse>} - The response object, potentially with a 'locale' cookie set.
+ */
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -5,15 +13,12 @@ export const middleware = async (request: NextRequest) => {
   const localeCookie = request.cookies.get('locale');
   const acceptLanguage = request.headers.get('accept-language');
   let locale: string | undefined = localeCookie?.value;
-  // If no locale cookie is set, determine the locale from the 'accept-language' header
   if (!locale) {
     locale = acceptLanguage?.split(',')[0].split('-')[0] || 'en';
     const response = NextResponse.next();
-    // Set the locale cookie for future requests
     response.cookies.set('locale', locale, { path: '/' });
     return response;
   }
-  // Continue with the request
   return NextResponse.next();
 }
 
