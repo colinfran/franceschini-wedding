e182cc4d41ae1d160c8ab27f280a9e4053353d8b
Colin Franceschini <hello@colinfran.com>
2025-01-15 13:20:14 -0800

    cleanup

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,11 +1,15 @@
+/**
+ * Generates a request configuration for internationalization (i18n) for this Next.js application.
+ * This function retrieves the 'accept-language' header from the incoming request to determine the preferred language of the user.
+ * It then extracts the primary language code (e.g., 'en' from 'en-US') and uses it to load the corresponding locale messages.
+ * @returns {Promise<{ locale: string, messages: any }>} An object containing the locale and the corresponding messages.
+ */
 import {getRequestConfig} from 'next-intl/server';
-import {cookies, headers} from 'next/headers';
+import {headers} from 'next/headers';
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
   const acceptLanguage = headers().get('accept-language');
-  const locale = (acceptLanguage?.split(',')[0].split('-')[0] || 'en'); // Default to 'en' if no locale is found 
+  const locale = (acceptLanguage?.split(',')[0].split('-')[0] || 'en');
   return {
     locale,
     messages: (await import(`../locales/${locale}.json`)).default
