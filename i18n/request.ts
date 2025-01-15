7f5014e727509115befa53e6e4760ec46cd55d5c
Colin Franceschini <hello@colinfran.com>
2025-01-15 15:17:22 -0800

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -6,12 +6,24 @@
  */
 import {getRequestConfig} from 'next-intl/server';
 import {headers} from 'next/headers';
+import enMessages from '../locales/en.json'; // Preload default locale messages
+import esMessages from '../locales/es.json'; // Preload other locales if common
+
+const localeMessages: any = {
+  en: enMessages,
+  es: esMessages
+  // Add other preloaded locales here if necessary
+};
+
 
 export default getRequestConfig(async () => {
   const acceptLanguage = headers().get('accept-language');
   const locale = (acceptLanguage?.split(',')[0].split('-')[0] || 'en');
+  const messages = localeMessages[locale] 
+    ? localeMessages[locale] 
+    : (await import(`../locales/${locale}.json`)).default;
   return {
     locale,
-    messages: (await import(`../locales/${locale}.json`)).default
+    messages
   };
 });
