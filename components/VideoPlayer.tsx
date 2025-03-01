38e8419f9316be34d688b7eb95b7760df2023354
Colin Franceschini <hello@colinfran.com>
2025-03-01 09:58:23 -0800

    cleanup

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -3,8 +3,12 @@ import Video from "next-video"
 
 const VideoPlayer: FC = () => {
   return (
-    <div className="video-width">
-      <Video src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw" />
+    <div className="video-width flex justify-center">
+      <Video
+        className="w-full max-w-lg"
+        src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw"
+        suppressHydrationWarning={true}
+      />
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
+import { withNextVideo } from "next-video/process";
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -22,4 +23,4 @@ const nextConfig = {
     ],
   },
 };
-export default withNextIntl(nextConfig);
+export default withNextVideo(withNextIntl(nextConfig), { folder: 'y' });
