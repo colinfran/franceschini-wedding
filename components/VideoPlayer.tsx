1661d5cf67bd45a61d842f1b1ff10f8e8f33c26c
Colin Franceschini <hello@colinfran.com>
2025-02-28 17:31:54 -0800

    cleanup

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -3,7 +3,7 @@ import Video from "next-video"
 
 const VideoPlayer: FC = () => {
   return (
-    <div className="video-width m-auto">
+    <div className="video-width">
       <Video src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw" />
     </div>
   )
