577525ded1a6b955ebee3a098f0ca5e1c8dca463
Colin Franceschini <hello@colinfran.com>
2025-03-01 18:37:33 -0800

    update

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,14 +1,13 @@
-/* eslint-disable jsx-a11y/media-has-caption */
-
 import React, { FC } from "react"
+import Video from "next-video"
+import proposalVideo from "../videos/proposal.mp4"
 
 const VideoPlayer: FC = () => {
   return (
     <div className="flex justify-center">
-      <video
+      <Video
         className="w-full max-w-xs"
-        src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw"
-        controls
+        src={proposalVideo}
       />
     </div>
   )
