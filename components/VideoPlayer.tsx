dcf89ee05041e932e5aa0090c3c08f1222780c7d
Colin Franceschini <hello@colinfran.com>
2025-03-01 21:12:03 -0800

    update

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react"
 import Video from "next-video"
-import proposalVideo from "../videos/proposal.mp4"
+// import proposalVideo from "../videos/Tahoe2024Vertical.mp4"
 
 const VideoPlayer: FC = () => {
   return (
     <div className="flex justify-center">
-      <Video className="w-full max-w-xs" src={proposalVideo} />
+      <Video className="w-full max-w-xs" src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw" />
     </div>
   )
 }
