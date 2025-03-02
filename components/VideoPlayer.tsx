a99b1857c52938a0c30e0ab08a001e91ce79c16c
Colin Franceschini <hello@colinfran.com>
2025-03-01 21:12:10 -0800

    update

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -5,7 +5,10 @@ import Video from "next-video"
 const VideoPlayer: FC = () => {
   return (
     <div className="flex justify-center">
-      <Video className="w-full max-w-xs" src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw" />
+      <Video
+        className="w-full max-w-xs"
+        src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw"
+      />
     </div>
   )
 }
