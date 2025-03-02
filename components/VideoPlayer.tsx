8bf9b5a3a764fafad8e18e8fd031e0e824b4f45e
Colin Franceschini <hello@colinfran.com>
2025-03-01 18:37:37 -0800

    Update VideoPlayer.tsx

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -5,10 +5,7 @@ import proposalVideo from "../videos/proposal.mp4"
 const VideoPlayer: FC = () => {
   return (
     <div className="flex justify-center">
-      <Video
-        className="w-full max-w-xs"
-        src={proposalVideo}
-      />
+      <Video className="w-full max-w-xs" src={proposalVideo} />
     </div>
   )
 }
