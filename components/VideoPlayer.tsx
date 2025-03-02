99932c5b19fd4daa9513ad3755b779a3737b3f32
Colin Franceschini <hello@colinfran.com>
2025-03-01 22:12:49 -0800

    update

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,13 +1,12 @@
-/* eslint-disable jsx-a11y/media-has-caption */
 import React, { FC } from "react"
-// import proposalVideo from "../videos/Tahoe2024Vertical.mp4"
+import Video from "next-video"
 
 const VideoPlayer: FC = () => {
   return (
     <div className="flex justify-center">
-      <video
+      <Video
         className="w-full max-w-xs"
-        src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChA7Hma02hVMF9Td0KwYWlmfeO47b5UGhz2DPug"
+        src="https://2xnbgz8473.ufs.sh/f/8xJfnpQsLChAchmho3wTlfkxeBIMgrntpPDjJuysYqQSGKhw"
       />
     </div>
   )
