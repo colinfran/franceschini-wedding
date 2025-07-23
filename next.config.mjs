cc6fc6dfd6a1ce5b8106511e891df301feb6253f
Colin Franceschini <hello@colinfran.com>
2025-07-22 23:53:09 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/photoboothpics",
+        destination: "https://www.dropbox.com/scl/fo/57u0ru4djco3ev5sfqhkf/APAfGdlabiYZcWq8FB92OwI?rlkey=ytsejiw23o600xlnowlsghnum&st=v77vauh9&dl=0",
+        permanent: true,
+      },
+      {
         source: "/api/fetch",
         destination: "/api/cron",
         permanent: true,
